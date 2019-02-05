73c3bb5b7c2ede80544f3748cbe09b7b342810fa
khushi <khushiverma988@gmail.com>
2019-02-04 20:04:59 -0500

    third commit

--- src/JavaPackage/display.java
+++ src/JavaPackage/display.java
@@ -1,58 +1,37 @@
 package JavaPackage;
+import sun.awt.geom.AreaOp;
+
 import javax.naming.Name;
 import java.util.Scanner;
 public class display {
         public static void main(String args[])
         {
+                  Scanner input=new Scanner(System.in);
+                  Car[] c1 = new Car[1];
+                  String temp;
+                  for(int i=0;i<c1.length;i++)
+                  {
+                          System.out.println("Make = ");
+                          temp=input.next();
+                          c1[i]=new Car();
+                          c1[i].setMake(temp);
+                  }
+                  for(int i=0;i<c1.length;i++)
+                    {
+                        System.out.println("Make : "+ c1[i].getMake());
+                    }
 
-         /*   Scanner sc=new Scanner(System.in);
-            System.out.println("Name =");
-            System.out.println(sc.next());
-                  Car c1 = new Car();
-                  c1.getCarName();
-                  c1.setCarName("khushi");
-                  c1.getCarType();
-                  c1.setCarType("Five Seater");
-                  c1.PrintMyData();
-                  FixedBasedPartTime fd1=new FixedBasedPartTime();
-                  fd1.getRate();
-                  fd1.setRate(21);
-                  fd1.getHoursWorked();
-                  fd1.setHoursWorked(20);
-                  fd1.getFixedAmount();
-                  fd1.setFixedAmount(12);
-                  fd1.PrintMyData();
-                  MotorCycle m1=new MotorCycle();
-                  m1.getMake();
-                  m1.setMake("BMW");
-                  m1.getPlate();
-                  m1.setPlate("ca-283-22");
-                  m1.getMotorcycleType();
-                  m1.setMotorcycleType("Racer");
-                  m1.PrintMyData();
-                  MotorCycle m2=new MotorCycle();
-                  CommissionBasedPartTime ct1= new CommissionBasedPartTime();
-                  ct1.getRate();
-                  ct1.setRate(30);
-                  ct1.getHoursWorked();
-                  ct1.setHoursWorked(10);
-                  ct1.getCommission();
-                  ct1.setCommission(20);
-                  ct1.ComissionPerCalcEarnings();
-                  ct1.PrintMyData();
-                  CommissionBasedPartTime ct2= new CommissionBasedPartTime();
-                  FullTime f1=new FullTime();
-                  f1.getSalary();
-                  f1.setSalary(200);
-                  f1.getBonus();
-                  f1.setBonus(20);
-                  f1.PrintMyData();
-                  FullTime f2=new FullTime();
-                  Intern n1= new Intern();
-                  n1.getSchoolName();
-                  n1.setSchoolName("Lambtoncollege");
-                  n1.PrintMyData();
-                  Intern n2= new Intern();
- */
-        }
+                  FixedBasedPartTime[] fd1=new FixedBasedPartTime[5];
+
+
+                  MotorCycle[] m1=new MotorCycle[5];
+
+                  CommissionBasedPartTime[] ct1= new CommissionBasedPartTime[5];
+
+                  FullTime[] f1=new FullTime[5];
+
+                  PartTime[] p1=new PartTime[5];
+
+                  Intern[] n1= new Intern[5];
+            }
 }
